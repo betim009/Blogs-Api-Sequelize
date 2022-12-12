d37f4ee10d420fb47d2051a82f820878a0abb78e
Alberto <betim009@gmail.com>
2022-12-11 21:27:57 -0300

    req 7 e 8

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const loginController = require('./controllers/login');
 const userController = require('./controllers/user');
+const categoryController = require('./controllers/category'); 
+
 const { validateLogin } = require('./middleware/loginValidate');
 const { validateUser, emailExist } = require('./middleware/userValidate');
 const { validateToken } = require('./auth/validateJWT');
@@ -14,6 +16,9 @@ app.use(express.json());
 app.get('/user', validateToken, userController.getUsers);
 app.get('/user/:id', validateToken, userController.getById);
 app.post('/user', emailExist, validateUser, userController.createUser);
+
 app.post('/login', validateLogin, loginController.login);
+
+app.post('/categories', validateToken, categoryController.createCategory);
 
 module.exports = app;
---
+++ src/controllers/category.js
@@ -0,0 +1,18 @@
+const categoryService = require('../services/category');
+
+const createCategory = async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        if (!name) return res.status(400).json({ message: '"name" is required' });
+        
+        const category = await categoryService.createCategory(name);
+        return res.status(201).json(category);
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro interno', error: err.message });
+    }
+};
+
+module.exports = {
+    createCategory,
+};
